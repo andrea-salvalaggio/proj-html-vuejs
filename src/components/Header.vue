c9a1728843b608df3e027db72693d231b79b2a5e
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-26 17:15:19 +0200

    topbar stilizzata

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,15 @@
     <header class="primary-bg">
         <div class="container">
             <div class="row">
-                <div class="col-12 d-flex justify-content-between text-white p-1">
+                <div class="col-12 d-flex justify-content-between p-1">
                     <div>
-                        <span>Telefono</span>
-                        <span>(001)88451234 &nbsp 88455438</span>
+                        <img class="icon-topbar" src="../assets/img/phone-icon-white.png" alt="Phone Icon">
+                        <span class="phone-info">(001)88451234 &nbsp &nbsp 88455438</span>
                     </div>
                     <div>
-                        <span>Facebook</span>
-                        <span>Twitter</span>
-                        <span>Pinterest</span>
+                        <img class="icon-topbar" src="../assets/img/fb-icon-white.png" alt="Facebook Icon">
+                        <img class="icon-topbar" src="../assets/img/twitter-icon-white.png" alt="Twitter Icon">
+                        <img class="icon-topbar" src="../assets/img/pinterest-icon-white.png" alt="Pinterest Icon">
                     </div>                
                 </div>
             </div>
@@ -27,8 +27,19 @@ export default {
 <style lang="scss" scoped>
 @import "../styles/variables.scss";
 
+    // Topbar
     .primary-bg{
         background-color: $primaryColorBg;
+    }
+
+    .icon-topbar{
+        width: 15px;
+        margin: 0 10px;
+    }
+
+    .phone-info{
+        font-size: 12px;
+        color: #ffff;
     }
 
 </style>
