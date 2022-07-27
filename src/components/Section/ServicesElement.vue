868903632f024c177f53667824564fb6305058f9
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-27 17:18:00 +0200

    sezione card definita

--- src/components/Section/ServicesElement.vue
+++ src/components/Section/ServicesElement.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="container">
+    <div class="container margin-tb">
         <div class="row">
             <div class="col-12 d-flex">
-                <div>
-                    <h1>We Are Here To Make Your Website Look More Elegant And Stylish!</h1>
+                <div class="services-container">
+                    <h1 class="mb-5">We Are Here To Make Your <span>Website</span> Look More <span>Elegant</span> And Stylish!</h1>
                     <div class="line-small line-gradient rounded-pill">
                         <div class="line-big line-gradient rounded-pill"></div>
                     </div>
-                    <button class="button button-primary">View all</button>
+                    <button class="button button-primary mt-5">View all</button>
                 </div>
                 <div>
                     <CardServices/>
                 </div>
-                <div>
+                <div class="side-text">
                     <span>17 years of experience helping people for best solutions</span>
                 </div>
             </div>
@@ -34,6 +34,19 @@ export default {
 
 <style lang="scss">
 @import "../../styles/variables.scss";
+
+    .services-container{
+        h1{
+            font-size: 50px;
+            font-weight: 600;
+            letter-spacing: -3px;
+            line-height: 60px;
+
+            span{
+                color: $tertiaryColorBrand;
+            }
+        }
+    }
 
     .line-small{
         width: 10px;
@@ -67,6 +80,18 @@ export default {
         &:hover{
             box-shadow: 0 8px 20px rgba($primaryColorBrand, $alpha: 0.3);
         }
+    }
+
+    .side-text{
+        transform: rotate(-180deg);
+        writing-mode:vertical-lr;
+        text-align: center;
+        margin-left: 25px;
+        font-size: 13px;
+    }
+
+    .margin-tb{
+        margin: 200px 0;
     }
 
 </style>
