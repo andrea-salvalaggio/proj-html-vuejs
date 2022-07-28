c26d524781fa3abab0bbe5000fe3e3c85c5bf6b0
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-28 12:14:20 +0200

    QuoteElement stilizzato

--- src/components/Section/QuoteElement.vue
+++ src/components/Section/QuoteElement.vue
@@ -2,15 +2,15 @@
     <div class="image-bg">
         <div class="container">
             <div class="row">
-                <div class="col-12 d-flex justify-content-center">
+                <div class="col-12">
                     <div class="quote-container">
-                        <h1></h1>
-                        <p></p>
-                        <div>
-                            <img src="" alt="">
+                        <img class="quote-icon" src="../../assets/img/quote-icon.svg" alt="Quote Icon">
+                        <p class="text-center mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod fugit ex illum sequi qui deleniti quidem sunt quisquam repellat, quis enim! Laborum totam voluptatibus obcaecati dolores commodi? Molestias, sint cum?</p>
+                        <div class="img-container mt-5">
+                            <img src="../../assets/img/businesswoman-analysing-document-P8WSNMC-1024x820.jpg" alt="">
                         </div>
                         <h6>Lissa Durbin</h6>
-                        <div>Client</div>
+                        <div class="type">Client</div>
                     </div>
                 </div>
             </div>
@@ -30,9 +30,65 @@ export default {
     .image-bg{
         background-image: url("../../assets/img/four-businesspeople-in-a-boardroom-with-paperwork-PC4V8H4.jpg");
         background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        padding: 200px 0;
+        position: relative;
+        z-index: 1;
+
+        &::after{
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba($color: rgba(50, 50, 50, 0), $alpha: 0.9);
+            z-index: -1;
+        }
     }
 
     .quote-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .quote-icon{
+            width: 50px;
+        }
+
+        .img-container{
+            width: 90px;
+            height: 90px;
+
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                border-radius: 50%;
+            }
+        }
+
+        .type{
+            font-size: 18px;
+            margin-top: -5px;
+            color: white;
+        }
+
+        p{
+            font-size: 24px;
+            font-style: italic;
+            color:white;
+        }
+
+        h6{
+            font-size: 22px;
+            font-weight: 600;
+            text-transform: uppercase;
+            margin-top: 25px;
+            color: $tertiaryColorBrand;
+        }
     }
+
 
 </style>
